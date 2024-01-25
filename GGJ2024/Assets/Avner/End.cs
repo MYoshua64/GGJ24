464a50a4a46818e562d941f1a9df8a3baead1918
GGJ24 <157756788+GGJ24@users.noreply.github.com>
2024-01-26 01:57:06 +0200

    End level protal
    added level portal

---
+++ GGJ2024/Assets/Avner/End.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Dictionary<string, bool> enteredEntities = new Dictionary<string, bool>
+    {
+        { "Monster", false },
+        { "Child", false }
+    };
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enteredEntities.ContainsKey(other.name))
+        {
+            enteredEntities[other.name] = true;
+        }
+
+        foreach (bool entered in enteredEntities.Values)
+        {
+            
+            if (!entered)
+            {
+                return;
+            }
+        }
+        Debug.Log("End of level!");
+        //trigger level end
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (enteredEntities.ContainsKey(other.name))
+        {
+            enteredEntities[other.name] = false;
+        }
+    }
+}
