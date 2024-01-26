d988d5df5d64faf551cc9e711ed61bf49cfcae1b
GGJ24 <157756788+GGJ24@users.noreply.github.com>
2024-01-26 16:37:08 +0200

    finito
    finito

---
+++ GGJ2024/Assets/Almog/Scripts/PauseManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu = null;
+
+    bool isPaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            Time.timeScale = isPaused ? 0 : 1;
+            pauseMenu.SetActive(isPaused);
+        }
+    }
+    public void ContinueGameManager()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+        pauseMenu.SetActive(isPaused);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("UI Afraid of the dark");
+    }
+}
