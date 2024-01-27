d0b2499c917bd3571a5f76ac0303e28d1a066b99
Hadi <hadidanial97@gmail.com>
2024-01-27 16:06:10 +0200

    Update Child.cs

--- GGJ2024/Assets/Almog/Scripts/Child.cs
+++ GGJ2024/Assets/Almog/Scripts/Child.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using DG.Tweening;
-using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
